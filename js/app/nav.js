b5bca89666b4ddb087c39e1a1163777b3fdad8b5
3rdVoyager <palebluedot316@gmail.com>
2026-09-10 21:46:37 -0400

    Add material fonts icons in sidebar.

--- js/app/nav.js
+++ js/app/nav.js
@@ -8,21 +8,21 @@ const sidebar = `
             <h1>OffScript!</h1>
         </header>
         <ul>
-            <li><a href="/app/index.html">Dashboard</a></li>
+            <li><a href="/app/index.html"><span class="material-icons" aria-hidden="true">dashboard</span>Dashboard</a></li>
         </ul>
         <div>
             <p class="sidebar-section-label">Current Script</p>
             <p class="sidebar-section-title">${loadedScript.title}</p>
             <ul>
-                <li><a href="/app/practice/index.html">Practice</a></li>
-                <li><a href="/app/progress/index.html">Progress</a></li>
+                <li><a href="/app/practice/index.html"><span class="material-icons" aria-hidden="true">mic</span>Practice</a></li>
+                <li><a href="/app/progress/index.html"><span class="material-icons" aria-hidden="true">trending_up</span>Progress</a></li>
             </ul>
         </div>
         <div class="nav-section--bottom">
             <p class="sidebar-section-label">Settings</p>
             <ul>
-                <li><a href="/app/scripts/index.html">Manage Scripts</a></li>
-                <li><a href="/app/settings/index.html">Settings</a></li>
+                <li><a href="/app/scripts/index.html"><span class="material-icons" aria-hidden="true">library_books</span>Manage Scripts</a></li>
+                <li><a href="/app/settings/index.html"><span class="material-icons" aria-hidden="true">settings</span>Settings</a></li>
             </ul>
         </div>
     </nav>
